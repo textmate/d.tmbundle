c9f33fa6b8626339c762899c8c0b30305b1367ba
Jacob Carlborg <doob@me.com>
2015-06-20 23:29:23 +0200

    Move Support/spec to spec/dsupport

--- Support/spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-SPEC_ROOT = File.dirname(__FILE__)
-$: << File.join(SPEC_ROOT, "..")
-Dir[SPEC_ROOT + "spec/support/**/*.rb"].each {|f| require f}
-
-RSpec.configure do |config|
-  config.order = "random"
-end
